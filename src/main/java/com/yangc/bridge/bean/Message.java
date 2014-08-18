65771da2bbcdff6f438d155f10bb0a78e82592c0
yangchenjava <yangchen_java@126.com>
2014-08-18 23:42:47 +0800

    bridge

---
+++ src/main/java/com/yangc/bridge/bean/Message.java
@@ -0,0 +1,51 @@
+package com.yangc.bridge.bean;
+
+public class Message {
+
+	private String id;
+	private String from;
+	private String to;
+	private String type;
+	private String body;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+}
